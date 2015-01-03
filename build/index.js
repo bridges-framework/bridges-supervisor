ace1714b725d27926c6cf3c669d8d3e7db13d4bd
Steven Zeiler <zeiler.steven@gmail.com>
2015-01-03 13:39:44 -0800

    [FEATURE] BridgesApplication with Routes and Controller

---
+++ build/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var express = require("express");
+var BridgesController = require("bridges-controller");
+var BridgesRoutes = require("bridges-routes");
+var fs = require("fs");
+var path = require("path");
+
+var BridgesApplication = function BridgesApplication(options) {
+  if (!fs.existsSync(options.root)) {
+    throw new Error("options.root must be a directory");
+  }
+  this.root = options.root;
+  this.server = express();
+
+  if (!options.controllers) {
+    options.controllers = { inject: [] };
+  }
+
+  var controllers = BridgesController.load({
+    directory: path.join(options.root, "/controllers"),
+    inject: options.controllers.inject
+  });
+
+  this.server.use("/v1", BridgesRoutes.draw({
+    controllers: controllers,
+    path: path.join(options.root, "config/routes")
+  }));
+
+  this.server.use(function (error, req, res, next) {
+    if (error) {
+      res.status(500).send({
+        success: false,
+        error: error.message
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = BridgesApplication;
