56390cd2818fd4ff02b4182d1ec3c8ba853c58a5
Adam Cmiel <adam@anypay.io>
2015-06-12 07:08:22 +0100

    [ESNEXT] update project to modern ECMAScript standards

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 import {Supervisor, Process} from 'domain-supervisor'
-import * as Promise          from 'bluebird'
-var requireAll = require('require-all-to-camel')
+import requireAll            from 'require-all-to-camel'
 
 class BridgesSupervisor {
 
-  constructor(options) {
+  constructor({directory, inject} = {inject: []}) {
     this._supervisor = new Supervisor()
-    this.processes   = requireAll(options.directory)
-    this.inject      = options.inject || []
-
-    this.onError = function(error, restart, crash) {
-      console.log('bridges:supervisor:error:message', error.message)
-      console.log('bridges:supervisor:error:stack'  , error.stack);
-      console.log('bridges:supervisor:restart')
-      restart();
-    }
+    this.processes   = requireAll(directory)
+    this.inject      = inject
   }
 
-  start() {
-    var _this = this
-    return new Promise(function(resolve, reject) {
-      var processes = []
-      try {
-        Object.keys(_this.processes).forEach(function(name) {
-          var proc  = new Process(function() {
-            _this.processes[name].apply(this, _this.inject)
-          })
-          processes.push(_this._supervisor.run(proc, _this.onError))
+  onError(error, restart, crash) {
+    console.log('bridges:supervisor:error:message', error.message)
+    console.log('bridges:supervisor:error:stack'  , error.stack)
+    console.log('bridges:supervisor:restart')
+    restart()
+  }
+
+  async start() {
+    return Object.keys(this.processes).map(name => {
+        var proc = new Process(() => {
+            this.processes[name].apply(null, this.inject)
         })
-      } catch (error) {
-        return reject(error)
-      }
-      resolve(processes)
+        return this.supervisor.run(proc, this.onError)
     })
   }
+
 }
 
 module.exports = BridgesSupervisor
